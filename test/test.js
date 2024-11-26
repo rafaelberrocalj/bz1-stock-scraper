c9205c899a9f111f3b80857ac7ed7fcaedb3235b
Rafael Berrocal Justiniano <rafaelberrocalj@gmail.com>
2024-11-26 09:28:14 -0300

    test

--- test/test.js
+++ test/test.js
@@ -3,10 +3,18 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto("https://investidor10.com.br/fiis/bari11/", {
     waitUntil: "networkidle2",
   });
-  await page.pdf({ path: "test.pdf", format: "a4" });
+  await page.pdf({ path: "investidor10.pdf", format: "a4" });
+  console.log("investidor10 ok");
+
+  await page.goto("https://statusinvest.com.br/fundos-imobiliarios/bari11", {
+    waitUntil: "networkidle2",
+  });
+  await page.pdf({ path: "statusinvest.pdf", format: "a4" });
+  console.log("statusinvest ok");
 
   await browser.close();
 })();
